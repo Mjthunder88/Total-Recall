9e45a219ebd8157ab542904fd6e921b45d278987
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2023-11-27 17:25:46 -0700

    got background images for cards but need to work on displaying the cards

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -2,7 +2,9 @@ import { Paper } from "@mui/material"
 
 function Card() {
   return (
-    <Paper>Card</Paper>
+    <Paper elevation={12} square={false} sx={{width: '200px', height: '200px', backgroundImage: 'url(../assets/santa-hat.png)', backgroundSize: 'fill'}}>
+      
+    </Paper>
   )
 }
 
--- src/screens/GameScreen.tsx
+++ src/screens/GameScreen.tsx
@@ -1,17 +1,23 @@
-import Card from "../components/cards/Card"
-import { Container, Button } from "@mui/material"
+import Card from "../components/cards/Card";
+import { Container, Button, Box, Paper } from "@mui/material";
 
 type GameProp = {
   showGameHandler: () => void;
-}
+};
 
-function GameScreen({showGameHandler}: GameProp) {
+function GameScreen({ showGameHandler }: GameProp) {
   return (
-    <Container>
+    <>
+      <Paper elevation={16} square={false} sx={{ minHeight: "80vh", minWidth: '80vw' }}>
         <Card />
-        <Button variant="contained" onClick={showGameHandler}>Start New Game</Button>
-    </Container>
-  )
+      </Paper>
+        <Box sx={{ textAlign: "center", padding: "1rem 0" }}>
+          <Button variant="contained" onClick={showGameHandler}>
+            Start New Game
+          </Button>
+        </Box>
+    </>
+  );
 }
 
-export default GameScreen
+export default GameScreen;
